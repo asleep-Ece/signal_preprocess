2c6c83efae3ae85a9af052a3a54c25ecbe2742ef
erik <erik.kim@asleep.ai>
2023-08-04 16:25:44 +0900

    Create get_edf_dir and load_psg_channel

--- psgsync.py
+++ psgsync.py
@@ -1,35 +1,72 @@
 import os
 import numpy as np
 import pandas as pd
+import mne
+from pyedflib import highlevel
+from glob import glob
 
-'''우리가 하려고 하는것
-1. psg signal의 start time과 sound의 label start time을 일단 맞춘다
-2. psg signal을 가지고 원하는 channel에 대한 데이터를 추출하고 
-3. 30초씩 잘라서 numpy로 저장 (nas/SNUBH-PSG_~/signal_extract)에다가 기존의 형식대로 그대로 디렉토리 만들어서 저장
-4. 30초씩 잘려진 sound data의 파일명이 0에서 1로 바뀐다면 그 사이의 애들은 버리고 그 뒤의 애들은 전부 rename
-''' 
 
-def load_psg_channel():
-    '''Load psg file with selected channels'''
-    pass
+def add_arguments(parser):
+    parser.add_argument('--sampling_rate', type=int, default=250, help='Downsampling frequency')
 
-def calculate_data_offset(psg_dir):
-    '''Cutoff the offset between PSG start time and label start time'''
-    pass
+    return parser
 
-def divide_psg_data():
-    '''divide psg data into 30s with considering the frequency'''
-    pass
+class PSG_split():
+    def __init__(self, parser):
+        super(PSG_split, self).__init__()
 
-def check_disconnection():
-    '''check whether there are disconnections by file name'''
-    pass
+        parser = add_arguments(parser)
+        self.args = parser.parse_args()
 
-def check_xml():
-    '''check start time of the disconnected xml file'''
-    pass
+        DATA_DIR = '/nas/SNUBH-PSG_signal_extract/'
+        OUTPUT_DIR = '/nas/SNUBH-PSG_signal_extract/signal_extract'
 
-def calculate_label_starttime():
-    '''Find the nearest 30x time from the start time of the xml file'''
-    pass
+    def get_edf_dir(self, patient_num, mode='train_data'):
+        # Get directory of the PSG edf file
+        sub_edf_path = os.path.join(DATA_DIR, mode, patient_num)
+        edf_dir = os.path.join(sub_edf_path, patient_num+'_signal', patient_num+'.edf')
+        
+        return edf_dir
+
+    def load_psg_channel(self, edf_dir, patient, channels):
+        # Load psg data with selected channels
+        signal_by_channel = []
+        signals = []
+        raw = mne.io.read_raw_edf(edf_dir)
+        for channel in channels:
+            raw_channel_signal = raw.get_data(channel)
+            signal_by_channel.append(raw_channel_signal)
+        signals = np.stack(signals, axis=0)
+
+        return signals
+
+    def calculate_data_offset(psg_dir):
+        '''Cutoff the offset between PSG start time and label start time'''
+        pass
+
+    def divide_psg_data():
+        '''
+        divide psg data into 30s with considering the frequency
+            for data_dir in os.listdir(DATA_DIR):
+            sub_edf_path = os.path.join(DATA_DIR, mode, data_dir)
+            if not os.path.isdir(sub_edf_path):
+                continue
+            patient_list = [os.path.join(sub_edf_path, x) for x in os.listdir(sub_edf_path)]
+            patient_list = [x for x in patient_list if os.path.isdir(x)]
+            if len(patient_list) == 0:
+                continue
+        '''
+        pass
+
+    def check_disconnection():
+        '''check whether there are disconnections by file name'''
+        pass
+
+    def check_xml():
+        '''check start time of the disconnected xml file'''
+        pass
+
+    def calculate_label_starttime():
+        '''Find the nearest 30x time from the start time of the xml file'''
+        pass
 
