3292a29b82a04d8e47eb34432b00b3c06a570a98
ece <ece.wu@asleep.ai>
2023-08-21 11:40:29 +0900

    add resample code

--- psgsync.py
+++ psgsync.py
@@ -11,6 +11,7 @@ import math
 import pickle
 import re
 import time
+import scipy
 
 parser = argparse.ArgumentParser(description="PSG data preprocess")
 
@@ -59,6 +60,7 @@ class PSG_split():
         
         '''
         epoch = 30
+        re_freq = 250
         psg_epochs = dict()
         #get the labels
         labels = pd.read_csv(label_dir,header=None).values
@@ -101,6 +103,7 @@ class PSG_split():
                     print(f"offset: {-flag}, red_labels {red_labels} rate {raw_rate}")
                     edd_off = len(raw_data)-len(temp_labels)*epoch*int(raw_rate)
                     raw_data = raw_data[:-edd_off]
+                    raw_data = scipy.signal.resample(raw_data,int(len(raw_data)/(raw_rate/re_freq)))
                     print(f"processed data: {len(raw_data)}")
                     
                 # divide into 30 seconds based on the number of labels
